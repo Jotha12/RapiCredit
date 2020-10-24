a5975e4e105d4614a9a9bb12fe84a72e2e9f0711
jcampeontec <jcampeontec@outlook.com>
2020-10-23 23:47:46 -0600

    jose3

---
+++ RapiCredit/Controllers/CrearCuentaClienteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RapiCredit.Controllers
+{
+    public class CrearCuentaClienteController : Controller
+    {
+        // GET: CrearCuentaCliente
+        public ActionResult CrearCuentaCliente()
+        {
+            return View();
+        }
+    }
+}
