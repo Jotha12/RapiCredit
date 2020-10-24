473f87c13cd8e4af4bc3f3917d6b5c92a0a872a6
jcampeontec <jcampeontec@outlook.com>
2020-10-23 23:10:59 -0600

    jose1

---
+++ RapiCredit/Controllers/AutorizacionDePrestamoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RapiCredit.Controllers
+{
+    public class AutorizacionDePrestamoController : Controller
+    {
+        // GET: AutoriazacionDePrestamo
+        public ActionResult AutorizacionDePrestamo()
+        {
+            return View();
+        }
+    }
+}
