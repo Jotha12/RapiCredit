6edff4dd4d08189b7b0b46dd7c936a2910dae47a
Alexis Jimenez <Alexis Jimenez@DESKTOP-OSG763A>
2020-10-26 00:15:30 -0600

    md tipo de prestamo

---
+++ RapiCredit/Controllers/PrestamoAmpliarController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RapiCredit.Controllers
+{
+    public class PrestamoAmpliarController : Controller
+    {
+        // GET: PrestamoAmpliar
+        public ActionResult PrestamoAmpliar()
+        {
+            return View();
+        }
+    }
+}
---
+++ RapiCredit/Controllers/PrestamoInicioController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RapiCredit.Controllers
+{
+    public class PrestamoInicioController : Controller
+    {
+        // GET: PrestamoInicio
+        public ActionResult PrestamoInicio()
+        {
+            return View();
+        }
+    }
+}
