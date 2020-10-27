f542db1d821164a02916decacc79557635fcf294
jcampeontec <jcampeontec@outlook.com>
2020-10-26 22:23:09 -0600

    jose7

---
+++ RapiCredit/Controllers/CuentaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RapiCredit.Controllers
+{
+    public class CuentaController : Controller
+    {
+        // GET: Cuenta
+        public ActionResult Cuenta()
+        {
+            return View();
+        }
+    }
+}
