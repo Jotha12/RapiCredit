ad9732e8214ab160b17a4b4a94482a1383185337
Jotha12 <72186230+Jotha12@users.noreply.github.com>
2020-10-27 01:44:19 -0600

    J6

--- RapiCredit/Controllers/SolicitudController.cs
+++ RapiCredit/Controllers/SolicitudController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using RapiCredit.Models;
+using RapiCredit.Models.ViewModels;
 
 namespace RapiCredit.Controllers
 {
@@ -11,7 +13,98 @@ namespace RapiCredit.Controllers
         // GET: Solicitud
         public ActionResult Solicitud()
         {
+            List<ViewSolicitud> lst;
+            using (RapiCreditEntities1 db = new RapiCreditEntities1())
+            {
+                 lst = (from d in db.Solicitud
+                           select new ViewSolicitud
+                           {
+                               Id = d.ID,
+                               PrimerNombre = d.PrimerNombre,
+                               SegundoNombre = d.SegundoNombre,
+                               PrimerApellido = d.PrimerApellido,
+                               SegundoApellido = d.SegundoApellido,
+                               DPI = d.DPI,
+
+
+
+
+                           }).ToList();
+            }
+
+                return View(lst);
+        }
+
+        // GET: Solicitud/Details/5
+        public ActionResult Details(int id)
+        {
             return View();
+        }
+
+        // GET: Solicitud/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Solicitud/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Solicitud/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Solicitud/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Solicitud/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Solicitud/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
---
+++ RapiCredit/Models/ViewModels/ViewSolicitud.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+
+namespace RapiCredit.Models.ViewModels
+{
+    public class ViewSolicitud
+    {
+  
+           public int Id {get; set; }
+           public string PrimerNombre { get; set; }
+        public string SegundoNombre { get; set; }
+        public string PrimerApellido { get; set; }
+        public string SegundoApellido { get; set; }
+        public string DPI { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public int Telefono { get; set; }
+        public string Correo { get; set; }
+        public int Monto { get; set; }
+        public int Cuotas { get; set; }
+
+
+
+
+
+    }
+}
