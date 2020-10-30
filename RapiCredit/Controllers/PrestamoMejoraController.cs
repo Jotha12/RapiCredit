1a1d115ab6e2fa65ed4097f1e5ac58b20df9c95f
Alexis Jimenez <Alexis Jimenez@DESKTOP-OSG763A>
2020-10-30 01:02:03 -0600

    md 01

---
+++ RapiCredit/Controllers/PrestamoMejoraController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RapiCredit.Controllers
+{
+    public class PrestamoMejoraController : Controller
+    {
+        // GET: PrestamoMejora
+        public ActionResult PrestamoMejora()
+        {
+            return View();
+        }
+    }
+}
